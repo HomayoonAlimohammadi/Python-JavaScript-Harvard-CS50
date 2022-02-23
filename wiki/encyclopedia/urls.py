d304a108efcc7ca92f71e4cfb073d206f1e12770
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-02-23 18:36:38 +0330

    Add Create

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views import (
     index,
     entry_view,
+    create_view
 )
 
 app_name = 'encyclopedia'
 urlpatterns = [
     path("", index, name="index"),
-    path('wiki/<str:title>/', entry_view, name='entry')
+    path('wiki/create/', create_view, name='create'),
+    path('wiki/<str:title>/', entry_view, name='entry'),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from matplotlib.pyplot import get
 from django.http import Http404
 from markdown import Markdown
-
+from .forms import (
+    CreateForm,
+)
 from .util import (
     list_entries,
     get_entry,
+    save_entry,
 )
 
 
@@ -17,7 +20,7 @@ def index(request):
 def entry_view(request, title):
     page_content = get_entry(title)
     if page_content is None:
-        return Http404
+        raise Http404
     else:
         page_content = Markdown().convert(page_content)
     context = {
@@ -25,4 +28,21 @@ def entry_view(request, title):
         'title': title,
     }
     return render(request, 'encyclopedia/entry.html', context = context)
+
+def search_results_view(request, search):
+
+    return
+
+
+def create_view(request):
+    if request.method == 'POST':
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            title, content = form.cleaned_data['title'], form.cleaned_data['content']
+            save_entry(title, content)
+    form = CreateForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'encyclopedia/create.html', context=context)
 
